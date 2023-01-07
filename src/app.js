2c0945376fe47dc628391026a54eee99346c46cc
noah <noah@rainbow.co.kr>
2023-01-08 00:43:07 +0900

    Vanilla JS - 아이템 케러셀 구상

--- src/app.js
+++
@@ -1,6 +0,0 @@
-var test = document.querySelector('.test');
-var testDom = document.createElement('article');
-testDom.textContent = 'this is typescript file';
-test === null || test === void 0 ? void 0 : test.appendChild(testDom);
-
-console.log(itemData);
--- src/app.ts
+++
@@ -1,7 +0,0 @@
-const test = document.querySelector('.test');
-
-const testDom = document.createElement('article');
-
-testDom.textContent = 'this is typescript file';
-
-test?.appendChild(testDom);
