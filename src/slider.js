b9159e7c043193cad151bd9a8b951d57c57bc2ad
noah <noah@rainbow.co.kr>
2023-01-09 18:34:10 +0900

    slide image 틀 생성

--- src/slider.js
+++ src/slider.js
@@ -1,9 +1,6 @@
 {
     /* <img src="https://cdn.pixabay.com/photo/2017/07/27/16/45/i-2545938_1280.jpg" />
-
 <img src="https://cdn.pixabay.com/photo/2018/04/26/08/01/food-3351333_1280.jpg" />
-
 <img src="https://cdn.pixabay.com/photo/2018/09/10/09/21/haejangguk-3666599_1280.jpg" />
-
 <img src="https://cdn.pixabay.com/photo/2015/03/31/06/20/asia-700611_1280.jpg" /> */
 }
