c63f964e356093ba69dfa039769f55e3795f6799
noah <noah@rainbow.co.kr>
2023-01-17 16:11:57 +0900

    파일 수정

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,13 @@ module.exports = {
     entry: {
         // 각 html에 필요한 entry 파일
         cart: "./src/cart.js",
-        main: "./src/mainApp.js",
+        mainApp: "./src/mainApp.js",
         menu: "./src/menu.js",
+        Data: "./lib/Data.js",
     },
     output: {
         path: __dirname + "/dist",
-        filename: "[name].bundle.js", // entry에 선언된 객체의 각 프로퍼티가 [name]과 치환되어 파일이 생성
+        filename: "[name].js", // entry에 선언된 객체의 각 프로퍼티가 [name]과 치환되어 파일이 생성
         // index.bundle.js, multiple.bundle.js
     },
     module: {
