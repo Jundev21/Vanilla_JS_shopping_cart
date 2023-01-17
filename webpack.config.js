3f66af49ce69235d6340ec4311960568f57a7d90
noah <noah@rainbow.co.kr>
2023-01-17 15:43:30 +0900

    htmlwebpack 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,26 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const webpack = require("webpack");
-const path = require("path");
+var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/app.js",
+    entry: {
+        // 각 html에 필요한 entry 파일
+        cart: "./src/cart.js",
+        main: "./src/mainApp.js",
+        menu: "./src/menu.js",
+    },
     output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
+        path: __dirname + "/dist",
+        filename: "[name].bundle.js", // entry에 선언된 객체의 각 프로퍼티가 [name]과 치환되어 파일이 생성
+        // index.bundle.js, multiple.bundle.js
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    devServer: {
-        client: {
-            overlay: true,
-        },
-        hot: true,
-        watchFiles: ["src/*", "index.html"],
+        rules: [{ test: /\.css$/, use: "css-loader" }],
     },
     plugins: [
-        new CopyWebpackPlugin({
-            patterns: ["index.html"],
+        new HtmlWebpackPlugin({
+            template1: "./index.html",
+            template2: "./cart.html",
+            template3: "/menu.html",
         }),
-        new webpack.HotModuleReplacementPlugin(),
     ],
 };
